1cc5b349805bd0668dc8ac729bb436d186aaf7cd
ViktorSimko <aqviktor@gmail.com>
2016-12-05 09:24:29 +0100

    added elephant

--- cs/NorbironTable/src/batfai/samuentropy/brainboard7/SplashActivity.java
+++ cs/NorbironTable/src/batfai/samuentropy/brainboard7/SplashActivity.java
@@ -39,58 +39,261 @@
  */
 package batfai.samuentropy.brainboard7;
 
-class Splash extends Thread {
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
 
-    SplashActivity act;
-    android.graphics.drawable.AnimationDrawable anim;
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.opengles.GL10;
 
-    public Splash(android.graphics.drawable.AnimationDrawable anim, SplashActivity act) {
-        this.anim = anim;
-        this.act = act;
-    }
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
-    public void st() {
-        android.content.Intent intent = new android.content.Intent();
-        intent.setClass(act, NeuronGameActivity.class);
-        act.startActivity(intent);
-        anim.stop();
-    }
-
-    @Override
-    public void run() {
-        anim.start();
-    }
-}
+import com.threed.jpct.*;
+import com.threed.jpct.util.MemoryHelper;
 
 /**
+ * A simple demo. This shows more how to use jPCT-AE than it shows how to write
+ * a proper application for Android. It includes basic activity management to
+ * handle pause and resume...
  *
- * @author nbatfai
+ * @author EgonOlsen
+ *
  */
-public class SplashActivity extends android.app.Activity {
+public class SplashActivity extends Activity {
 
-    private Splash splash;
+	// Used to handle pause and resume...
+	private static SplashActivity master = null;
 
-    @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+	private GLSurfaceView mGLView;
+	private MyRenderer renderer = null;
+	private FrameBuffer fb = null;
+	private World world = null;
+	private RGBColor back = new RGBColor(50, 50, 100);
 
-        android.widget.ImageView iv = (android.widget.ImageView) findViewById(R.id.neuronanimation);
+	private float touchTurn = 0;
+	private float touchTurnUp = 0;
 
-        iv.setBackgroundResource(R.drawable.neuron_animation);
+	private float xpos = -1;
+	private float ypos = -1;
 
-        final android.graphics.drawable.AnimationDrawable anim = (android.graphics.drawable.AnimationDrawable) iv.getBackground();
+	private Object3D cube = null;
+    private Object3D elephant = null;
+	private int fps = 0;
 
-        splash = new Splash(anim, this);
-        runOnUiThread(splash);
+	private Light sun = null;
 
-    }
+	protected void onCreate(Bundle savedInstanceState) {
 
-    @Override
-    public boolean onTouchEvent(android.view.MotionEvent evt) {
-        if (evt.getAction() == android.view.MotionEvent.ACTION_DOWN) {
-            splash.st();
+		Logger.log("onCreate");
+
+		if (master != null) {
+			copy(master);
+		}
+
+		super.onCreate(savedInstanceState);
+		mGLView = new GLSurfaceView(getApplication());
+
+		mGLView.setEGLConfigChooser(new GLSurfaceView.EGLConfigChooser() {
+			public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display) {
+				// Ensure that we get a 16bit framebuffer. Otherwise, we'll fall
+				// back to Pixelflinger on some device (read: Samsung I7500)
+				int[] attributes = new int[] { EGL10.EGL_DEPTH_SIZE, 16, EGL10.EGL_NONE };
+				EGLConfig[] configs = new EGLConfig[1];
+				int[] result = new int[1];
+				egl.eglChooseConfig(display, attributes, configs, 1, result);
+				return configs[0];
+			}
+		});
+
+		renderer = new MyRenderer();
+		mGLView.setRenderer(renderer);
+		setContentView(mGLView);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mGLView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mGLView.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	private void copy(Object src) {
+		try {
+			Logger.log("Copying data from master Activity!");
+			Field[] fs = src.getClass().getDeclaredFields();
+			for (Field f : fs) {
+				f.setAccessible(true);
+				f.set(this, f.get(src));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public boolean onTouchEvent(MotionEvent me) {
+
+		if (me.getAction() == MotionEvent.ACTION_DOWN) {
+			xpos = me.getX();
+			ypos = me.getY();
+			return true;
+		}
+
+		if (me.getAction() == MotionEvent.ACTION_UP) {
+			xpos = -1;
+			ypos = -1;
+			touchTurn = 0;
+			touchTurnUp = 0;
+			return true;
+		}
+
+		if (me.getAction() == MotionEvent.ACTION_MOVE) {
+			float xd = me.getX() - xpos;
+			float yd = me.getY() - ypos;
+
+			xpos = me.getX();
+			ypos = me.getY();
+
+			touchTurn = xd / -100f;
+			touchTurnUp = yd / -100f;
+			return true;
+		}
+
+		try {
+			Thread.sleep(15);
+		} catch (Exception e) {
+			// No need for this...
+		}
+
+		return super.onTouchEvent(me);
+	}
+
+	protected boolean isFullscreenOpaque() {
+		return true;
+	}
+
+	class MyRenderer implements GLSurfaceView.Renderer {
+
+		private long time = System.currentTimeMillis();
+
+		public MyRenderer() {
+		}
+
+        private Object3D loadModel(InputStream file, float scale) {
+
+            Object3D[] model = Loader.load3DS(file, scale);
+            Object3D o3d = new Object3D(0);
+            Object3D temp = null;
+            for (int i = 0; i < model.length; i++) {
+                temp = model[i];
+                temp.setCenter(SimpleVector.ORIGIN);
+                temp.rotateX((float)( -.5*Math.PI));
+                temp.rotateMesh();
+                temp.setRotationMatrix(new Matrix());
+                o3d = Object3D.mergeObjects(o3d, temp);
+                o3d.build();
+            }
+            return o3d;
         }
-        return true;
-    }
+
+		public void onSurfaceChanged(GL10 gl, int w, int h) {
+			if (fb != null) {
+				fb.dispose();
+			}
+			fb = new FrameBuffer(gl, w, h);
+
+			if (master == null) {
+
+				world = new World();
+				world.setAmbientLight(20, 20, 20);
+
+				sun = new Light(world);
+				sun.setIntensity(250, 250, 250);
+
+                String fileName = "res/elephant.obj";
+
+                File obj = new File(fileName);
+                if (!obj.exists()) {
+                    Log.i("sdad", "obj not exists");
+                }
+
+                InputStream objInputStream = getResources().openRawResource(R.raw.elephant);
+                elephant = loadModel(objInputStream, 1);
+
+				/*
+				// Create a texture out of the icon...:-)
+				Texture texture = new Texture(BitmapHelper.rescale(BitmapHelper.convert(getResources().getDrawable(R.drawable.icon)), 64, 64));
+				TextureManager.getInstance().addTexture("texture", texture);
+
+				cube = Primitives.getCube(10);
+				cube.calcTextureWrapSpherical();
+				cube.setTexture("texture");
+				cube.strip();
+				cube.build();
+				*/
+                elephant.build();
+				world.addObject(elephant);
+
+				Camera cam = world.getCamera();
+				cam.moveCamera(Camera.CAMERA_MOVEOUT, 25);
+				cam.lookAt(elephant.getTransformedCenter());
+
+				SimpleVector sv = new SimpleVector();
+				sv.set(elephant.getTransformedCenter());
+				sv.y -= 100;
+				sv.z -= 100;
+				sun.setPosition(sv);
+				MemoryHelper.compact();
+
+				if (master == null) {
+					Logger.log("Saving master Activity!");
+					master = SplashActivity.this;
+				}
+			}
+		}
+
+		public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		}
+
+		public void onDrawFrame(GL10 gl) {
+			if (touchTurn != 0) {
+				elephant.rotateY(touchTurn);
+				touchTurn = 0;
+			}
+
+			if (touchTurnUp != 0) {
+				elephant.rotateX(touchTurnUp);
+				touchTurnUp = 0;
+			}
+
+			fb.clear(back);
+			world.renderScene(fb);
+			world.draw(fb);
+			fb.display();
+
+			if (System.currentTimeMillis() - time >= 1000) {
+				Logger.log(fps + "fps");
+				fps = 0;
+				time = System.currentTimeMillis();
+			}
+			fps++;
+		}
+	}
 }
